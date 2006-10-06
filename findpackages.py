446ea0cd3ab4ac5470be54a2f0035bd3d8659981
jjneely@EOS.NCSU.EDU <jjneely@EOS.NCSU.EDU@35bbe6b0-b001-0410-9f0e-dd3f93706181>
2006-10-06 04:07:54 +0000

    Nothing to see here...move along.
    git-svn-id: https://svn.linux.ncsu.edu/svn/cls/trunk/rhntools@1931 35bbe6b0-b001-0410-9f0e-dd3f93706181

--- findpackages.py
+++ findpackages.py
@@ -3,11 +3,133 @@
 import os
 import os.path
 import config
+import string
+from types import IntType, FloatType, StringType
 from rhnapi import RHNClient
 
 TreeLocation = "/export/trees"
 PackageRoot = "/var/satellite/redhat"
 PackageDirs = os.listdir(PackageRoot)
+
+# Stolen from Yum
+def stringToVersion(verstring):
+    if verstring is None:
+        return ('0', None, None)
+    i = string.find(verstring, ':')
+    if i != -1:
+        try:
+            epoch = string.atol(verstring[:i])
+        except ValueError:
+            # look, garbage in the epoch field, how fun, kill it
+            epoch = '0' # this is our fallback, deal
+    else:
+        epoch = '0'
+    j = string.find(verstring, '-')
+    if j != -1:
+        if verstring[i + 1:j] == '':
+            version = None
+        else:
+            version = verstring[i + 1:j]
+        release = verstring[j + 1:]
+    else:
+        if verstring[i + 1:] == '':
+            version = None
+        else:
+            version = verstring[i + 1:]
+        release = None
+    return (epoch, version, release)
+
+def bruteForceFind(p):
+    epsilon = 0.0001
+    for dir in PackageDirs:
+        if dir.startswith('.'):
+            continue
+
+        namedir = os.path.join(PackageRoot, dir, p['package_name'])
+        if not os.path.exists(namedir):
+            continue
+
+        for evr in [os.path.basename(i) for i in os.listdir(namedir)]:
+            e, v, r = stringToVersion(evr)
+
+            # Epoch
+            if p['package_epoch'] == "" and e in [0, "0", ""]:
+                pass
+            elif p['package_epoch'] == int(e):
+                pass
+            else:
+                continue
+
+            # Version
+            if isinstance(p['package_version'], StringType):
+                if v == p['package_version']:
+                    pass
+                else:
+                    continue
+            elif isinstance(p['package_version'], IntType):
+                try: i = int(v)
+                except ValueError: continue
+                if p['package_version'] == i:
+                    pass
+                else:
+                    continue
+            elif isinstance(p['package_version'], FloatType):
+                try: f = float(v)
+                except ValueError: continue
+                if f - epsilon <= p['package_version'] <= f + epsilon:
+                    pass
+                else:
+                    continue
+
+            # Release
+            if isinstance(p['package_release'], StringType):
+                if r == p['package_release']:
+                    pass
+                else:
+                    continue
+            elif isinstance(p['package_release'], IntType):
+                try: i = int(r)
+                except ValueError: continue
+                if p['package_release'] == i:
+                    pass
+                else:
+                    continue
+            elif isinstance(p['package_release'], FloatType):
+                try: f = float(r)
+                except ValueError: continue
+                if f - epsilon <= p['package_release'] <= f + epsilon:
+                    pass
+                else:
+                    continue
+
+            # If we are here then we have a directory name that matched
+            bindir = os.path.join(namedir, evr, p['package_arch_name'])
+            srcdir = os.path.join(namedir, evr, 'SRPMS/')
+            binpath = None
+            srcpath = None
+
+            # Okay, there should be ONE rpm file here
+            if not os.access(bindir, os.R_OK):
+                print "Directory not found: %s" % bindir
+                print "Found packages but arch directory missing?"
+                continue
+            for file in os.listdir(bindir):
+                if file.endswith("%(package_arch_name)s.rpm" % p):
+                    binpath = os.path.join(bindir, file)
+                    print binpath
+                    break
+
+            # and one src rpm file
+            if os.access(srcdir, os.R_OK):
+                for file in os.listdir(srcdir):
+                    if file.endswith("src.rpm"):
+                        srcpath = os.path.join(srcdir, file)
+                        break
+
+            return binpath, srcpath
+    
+    print "Error:  Could not find packages: %s" % str(p)
+    return None, None
 
 def findRPM(path):
     for dir in PackageDirs:
@@ -40,7 +162,7 @@ def buildTreeUsing(label, rpm, srpm):
         os.makedirs(dir, 0755)
     if not os.path.exists(location):
         os.symlink(srpm, location)
-
+    
 def main():
     rhncfg = config.RHNConfig()
     rhn = RHNClient(rhncfg.getURL())
@@ -53,13 +175,22 @@ def main():
                                                   chan['channel_label'])
 
         for p in packages:
-            rpm = "%(package_name)s/%(package_version)s-%(package_release)s/%(package_arch_name)s/%(package_name)s-%(package_version)s-%(package_release)s.%(package_arch_name)s.rpm" % p
-            srpm = "%(package_name)s/%(package_version)s-%(package_release)s/SRPMS/%(package_name)s-%(package_version)s-%(package_release)s.src.rpm" % p
-            
-            location = findRPM(rpm)
-            source = findRPM(srpm)
-            if location is None:
-                print "Error: Could not find binary package: %s" % rpm
+            #rpm, srpm = buildPath(p, False)
+            #location = findRPM(rpm)
+            #source = findRPM(srpm)
+            #if location == None:
+            #    rpm, srpm = buildPath(p, True)
+            #    location = findRPM(rpm)
+            #    source = findRPM(srpm)
+
+            location, source = bruteForceFind(p)
+
+            if location == None:
+                print "Error: Could not find binary package:"
+                print p
             else:
                 buildTreeUsing(chan['channel_label'], location, source)
+
+if __name__ == "__main__":
+    main()
 
