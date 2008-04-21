6f52043cf812fbc6f84cc8517afb6924108788b2
jjneely@EOS.NCSU.EDU <jjneely@EOS.NCSU.EDU@35bbe6b0-b001-0410-9f0e-dd3f93706181>
2008-04-21 18:28:55 +0000

    Support excluding systems based on what groups they are a member of.
    git-svn-id: https://svn.linux.ncsu.edu/svn/cls/trunk/rhntools@2668 35bbe6b0-b001-0410-9f0e-dd3f93706181

--- oldSystems.py
+++ oldSystems.py
@@ -18,10 +18,12 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
+import re
 import sys
 import xmlrpclib
 import time
 import optparse
+from sets import Set
 from datetime import date
 from datetime import timedelta
 from rhnapi import RHNClient
@@ -53,6 +55,9 @@ def cliOptions():
 
     parser.add_option("-d", "--days", action="store", default=30,
                       type="int", dest="days", help="Your RHN server.")
+    parser.add_option("-e", "--exclude", action="store", default=None,
+                      type="string", dest="exclude",
+                      help="Regex of groups to ingore.")
     parser.add_option("--delete", action="store_true", default=False,
                       dest="delete", 
                       help="Delete these registrations from RHN.")
@@ -74,7 +79,17 @@ def cliOptions():
     opts.server = args[1]
     return opts
 
-def search(rhn, days):
+def getGroups(rhn, serverid):
+    groups = rhn.server.system.listGroups(rhn.session, serverid)
+    list = []
+
+    for group in groups:
+        if int(group['subscribed']) != 0:
+            list.append(group['system_group_name'])
+
+    return list
+
+def search(rhn, days, excludes):
     s = rhn.server
     delta = timedelta(days=days)
     today = date.today()
@@ -82,8 +97,12 @@ def search(rhn, days):
     systems = s.system.list_user_systems(rhn.session)
 
     for system in systems:
-        #sys.stderr.write("Working on: %s  ID: %s\n" % \
-        #                 (system["name"], system["id"]))
+        if len(excludes) > 0:
+            subed = getGroups(rhn, int(system['id']))
+            intersection = Set(subed).intersection(excludes)
+            if len(intersection) > 0:
+                print "Excluding system: %s" % system['name']
+                continue
 
         d = parseDate(system["last_checkin"])
         if today - delta > d:
@@ -104,6 +123,25 @@ def delete(rhn, list, noconfirm=False):
         else:
             print "Skipping %s" % server["name"]
 
+def findGroups(rhn, regex):
+    # Return groups that match regex
+    # We do this by grabbing a registered system and listing its groups.
+    # this gives us a list of all groups and if the system is subscribed
+    # from which we build a new list of names that match the regex.
+
+    systems = rhn.server.system.list_user_systems(rhn.session)
+    if len(systems) == 0:
+        raise StandardError("No systems subscribed to RHN.")
+
+    groups = rhn.server.system.listGroups(rhn.session, int(systems[0]['id']))
+    list = []
+    for group in groups:
+        match = regex.match(group['system_group_name'])
+        if match != None:
+            list.append(group['system_group_name'])
+
+    return list
+
 def main():
 
     print "Search and Destroy old RHN registrations."
@@ -118,7 +156,19 @@ def main():
     print "Today's date = %s" % date.today().isoformat()
     print
 
-    list = search(rhn, o.days)
+    if o.exclude:
+        try:
+            regex = re.compile(o.exclude)
+            excludes = findGroups(rhn, regex)
+        except re.error:
+            print "Regular expression error: %s" % o.exclude
+            sys.exit(1)
+        print "Excluding Groups: %s" % excludes
+    else:
+        excludes = []
+
+
+    list = search(rhn, o.days, excludes)
     for s in list:
         print s["name"]
 
